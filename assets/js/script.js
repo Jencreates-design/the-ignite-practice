d91712eebdfe26f0b9ae7a9f16f9af453a9cc6f2
Jencreates-design <mariajenniferelias@outlook.com>
2026-07-11 22:08:24 +1000

    Add hamburger menu toggle functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,15 @@ document.addEventListener("scroll", function () {
         header.classList.remove("scrolled");
     }
 });
+
+// ================================
+// HAMBURGER MENU
+// ================================
+
+const hamburger = document.querySelector(".hamburger");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobileMenu.classList.toggle("active");
+});
